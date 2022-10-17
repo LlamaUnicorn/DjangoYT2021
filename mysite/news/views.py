8dbbc9ee2dde9fda75b9c0a806b822adcc4e0142
LlamaUnicorn <holidaynight@gmail.com>
2022-10-17 09:37:17 +0300

    Аштшырув 11. Templates (tags, filters, context)

--- mysite/news/views.py
+++ mysite/news/views.py
@@ -1,11 +1,29 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import News
 
+# Pre 11. Templates
+# def index(request):
+#     # print(request)
+#     news = News.objects.all()
+#     res = '<h1>Список новостей</h1>'
+#     for item in news:
+#         res += f'<div>\n<p>{item.title}</p>\n<p>{item.content}</p>\n</div>\n<hr>\n'
+#     return HttpResponse(res)
+
+
+# Tiny context
+# def index(request):
+#     news = News.objects.all()
+#     return render(request, 'news/index.html', {'news': news, 'title': 'Список новостей'})
+
+# Huge context
 def index(request):
-    # print(request)
-    return HttpResponse('Hello world!')
+    news = News.objects.order_by('-created_at')
+    context = {
+        'news': news,
+        'title': 'Список новостей',
+    }
+    return render(request, template_name='news/index.html', context=context)
 
-
-def test(request):
-    return HttpResponse('<h1>Тестовая страница</h1>')
